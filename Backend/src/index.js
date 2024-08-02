f7d7d038f59cf3f9ae1e9590bb9578a9dd7d6653
Bisal Kumar <kumarmodibisal@gmail.com>
2024-08-02 11:44:52 +0530

    remove unnecessary code

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -2,15 +2,4 @@ import dotenv from "dotenv";
 dotenv.config({
   path: "./.env",
 });
-import connectDb from "../db/index.js";
-import app from "./app.js";
 
-connectDb()
-  .then(() => {
-    app.listen(process.env.PORT || 8000, () => {
-      console.log(`Server is running at port : ${process.env.PORT}`);
-    });
-  })
-  .catch((err) => {
-    console.log("MongoDB connection failed !!! ", err);
-  });
